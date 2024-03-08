07f8964a0128f2a789085e6ec510ac41dbc5e903
Mohammad Moroodi <mtahamoroodi@gmail.com>
2024-03-08 15:11:25 +0330

    create world chart api states in toolkit

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -13,3 +13,17 @@ const options = {
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
+
+    export const shazamCoreApi = createApi({
+      reducerPath : "shazamCoreApi",
+      baseQuery : fetchBaseQuery({
+        baseUrl : "https://shazam.p.rapidapi.com",
+        prepareHeaders : (headers) => {
+          headers.set("X-RapidAPI-Key" , "a53fa213e9mshba9f94cc489a062p13b193jsn1ef525c60bdb")
+          return headers;
+        },
+      }),
+      endpoints : (builder) => {
+        getWorldChart : builder.query({query : "/charts/track"})
+      }
+    }) 
